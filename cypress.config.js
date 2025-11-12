b42a7ec79b02d098f0d8f1eee0e806d0fae0731a
Claudio Bianco <claudiombianco@gmail.com>
2025-11-12 15:59:07 -0300

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: process.env.APP_BASE_URL || 'http://localhost:8080',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+    defaultCommandTimeout: 8000,   // aumentar se necessário
+    requestTimeout: 10000,
+    retries: {
+      runMode: 2,   // 2 tentativas extras no CI
+      openMode: 0,
     },
+    video: true,
+    screenshotOnRunFailure: true,
   },
 });
