673283e08cb162e72ed188f24dedf82ff1b20a64
Claudio Bianco <claudiombianco@gmail.com>
2025-11-12 18:33:50 -0300

    first commit

--- src/test/java/com/claudiobianco/java/renderapi/web/ItemControllerIT.java
+++ src/test/java/com/claudiobianco/java/renderapi/web/ItemControllerIT.java
@@ -1,4 +1,4 @@
-package com.claudiobianco.java.renderapi.api;
+package com.claudiobianco.java.renderapi.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
